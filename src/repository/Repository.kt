57f1af90d3e4ecd18b8c288c4b7c95a46d0e27a3
Sergey <svlassiev@gmail.com>
2019-09-22 09:56:42 +0200

    Update mongo configuration.

--- src/repository/Repository.kt
+++ src/repository/Repository.kt
@@ -1,7 +1,6 @@
 package info.vlassiev.serg.repository
 
 import com.mongodb.ConnectionString
-import com.mongodb.ServerAddress
 import info.vlassiev.serg.image.extractImageMetadata
 import info.vlassiev.serg.model.Image
 import info.vlassiev.serg.model.ImageList
@@ -9,12 +8,10 @@ import info.vlassiev.serg.model.getFolders
 import org.litote.kmongo.KMongo
 import org.litote.kmongo.getCollection
 import org.slf4j.LoggerFactory
-import java.net.InetAddress
 
 class Repository() {
     private val logger = LoggerFactory.getLogger(javaClass)
-    private val connectionString = ConnectionString("mongodb://mongo-0.mongo,mongo-1.mongo,mongo-2.mongo:27017/?replicaSet=test&readPreference=primaryPreferred&connectTimeoutMS=80000&socketTimeoutMS=20000")
-    private val serverAddress = ServerAddress(InetAddress.getLocalHost(), 27017)
+    private val connectionString = ConnectionString("mongodb://mongo-cdc-0.mongo-cdc,mongo-cdc-1.mongo-cdc,mongo-cdc-2.mongo-cdc:27017/?replicaSet=rs0&readPreference=primaryPreferred&connectTimeoutMS=80000&socketTimeoutMS=20000")
     private val client = KMongo.createClient(connectionString)
     private val database = client.getDatabase("colorless-days-children")
     private val imageListsCollection = database.getCollection<ImageList>()
@@ -38,7 +35,7 @@ fun spinUp() {
     logger.info("Starting spin up")
     val repository = Repository()
     getFolders().forEach() { folder ->
-        val images = folder.images.map { image -> extractImageMetadata("Baikal/Picture${image.imageId}.jpg", image) }
+        val images = folder.images.map { image -> extractImageMetadata("${folder.listId}/Picture${image.imageId}.jpg", image) }
         val imageList = ImageList(name = folder.name, images = images.map { it.imageId })
         logger.info("Starting spin up for folder ${folder.name}")
         repository.insertImageList(imageList)
