1cc810a3697eaba1875b1d758f6bfbfcf57d523b
Sergey <svlassiev@gmail.com>
2020-11-09 23:31:52 +0100

    Add image variants and return V1024 as default image source.

--- src/api/View.kt
+++ src/api/View.kt
@@ -1,6 +1,7 @@
 package info.vlassiev.serg.api
 
 import info.vlassiev.serg.image.ImageClient
+import info.vlassiev.serg.model.VariantName.V1024
 import io.ktor.application.call
 import io.ktor.request.receive
 import io.ktor.response.respond
@@ -13,7 +14,8 @@ fun Routing.viewApi(path: String, imageClient: ImageClient): Route {
         }
         post("/images") {
             val request = call.receive<ImagesRequest>()
-            call.respond(imageClient.findImages(request.imageIds, request.skip, request.limit))
+            call.respond(imageClient.findImages(request.imageIds, request.skip, request.limit)
+                .map { it.copy(location = it.variants.find { v -> v.name == V1024 }?.location ?: it.location) })
         }
         get("/timeline/data") {
             call.respond(imageClient.getTimelineData())
--- src/model/Images.kt
+++ src/model/Images.kt
@@ -3,7 +3,9 @@ package info.vlassiev.serg.model
 import java.time.Instant.now
 import java.util.*
 
-data class Image(val imageId: String, val location: String, val thumbnail: String, val description: String, val timestamp: Long, val gps: GpsData? = null)
+data class Image(val imageId: String, val location: String, val thumbnail: String, val description: String, val timestamp: Long, val gps: GpsData? = null, val variants: List<ImageVariant> = emptyList())
+data class ImageVariant(val name: VariantName, val location: String)
+enum class VariantName { DEFAULT, THUMBNAIL, V2048, V1024 }
 data class ImageList(val listId: String = UUID.randomUUID().toString(), val name: String, val images: List<String>)
 data class Folder(val listId: String, val name: String, val images: List<Image>, val prefix: String = "Picture", val postfix: String = ".jpg") {
     constructor(listId: String, name: String, count: Int, prefix: String = "Picture", postfix: String = ".jpg") : this(listId, name, getImagesForFolder(listId, count, prefix, postfix), prefix, postfix)
